c482f40e1874607bef714af9adb6b3601d6acf4b
+ <+>
2021-07-12 19:59:56 -0400

    Complete

--- index.js
+++ index.js
@@ -11,6 +11,15 @@ const tutorials = [
   'what is JSONP?'
 ];
 
-const titleCased = () => {
-  return tutorials
+const titleCased = tutorial => {
+  return tutorials.map(makeTitleCase)
 }
+
+function makeTitleCase(sentence){
+  let modifiedSentence = sentence.split(' ')
+  //console.log(modifiedSentence)
+  return modifiedSentence
+    .map(word => word[0].toUpperCase()+word.slice(1,word.length))
+    .join(' ')}
+
+titleCased(makeTitleCase)
